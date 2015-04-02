c0c35862399da45b75cc62cb9118ca799d1ed6ad
thiagokroger <thiagokroger@gmail.com>
2015-04-02 16:02:12 -0300

    go

--- auth0.js
+++ auth0.js
@@ -8,7 +8,12 @@ var api = new Auth0({
 	clientID: config.auth0ClientID,
 	clientSecret: config.auth0ClientSecret
 });
-
+AWS.config.update({
+	accessKeyId: config.AmazonAccessKeyId,
+	secretAccessKey: config.AmazonSecretAccessKey,
+	region: config.AmazonRegion
+});
+var dynamodb = new AWS.DynamoDB();
 
 module.exports = {
     signup: function (req, res, next) {
@@ -24,12 +29,74 @@ module.exports = {
 		            if (err) {
 			            console.log('Error creating user: ' + err);
 			            res.json(401, {erro:"Unable to save"});
+		            }else{
+			            var plan = require("./plans.json");
+			            plan = plan[101];
+
+			            var credits = plan.credits;
+			            var d = new Date();
+			            d.setMonth(d.getMonth() + parseInt(plan.periodicity));
+			            var expiration = Math.floor(d.getTime() / 1000).toString();
+			            var timestamp = Math.floor(Number(new Date()) / 1000).toString();
+			            var params = {
+				            TableName: 'Users',
+				            Key: {
+					            "UserEmail": {
+						            "S": req.body.email
+					            }
+				            },
+				            AttributeUpdates: {
+					            "IsSubscribed": {
+						            Action: 'PUT',
+						            Value: {
+							            N: '1'
+						            }
+					            },
+					            "LastWritten": {
+						            Action: 'PUT',
+						            Value: {
+							            N: timestamp
+						            }
+					            },
+					            "Data": {
+						            Action: 'PUT',
+						            Value: {
+							            S: "{}"
+						            }
+					            },
+					            "SubscriptionExpirationDate": {
+						            Action: 'PUT',
+						            Value: {
+							            N: expiration
+						            }
+					            },
+					            "LastModifiedBy": {
+						            Action: 'PUT',
+						            Value: {
+							            S: 'web'
+						            }
+					            },
+					            "CreditBalance": {
+						            Action: 'PUT',
+						            Value: {
+							            N: credits
+						            }
+					            }
+
+				            },
+				            ReturnValues: 'UPDATED_NEW'
+			            };
+
+			            dynamodb.updateItem(params, function(err, data) {
+				            if (err){
+					            res.send(401, err);
+				            }
+				            else{
+					            res.send(200);
+				            }
+			            });
 		            }
-
-		            res.send(200);
 	            });
-
-
 
             }else{
                 res.json(401, {erro:"Dados inválidos"});
